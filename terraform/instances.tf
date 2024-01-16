5d7c4c4c1440bf1bbf3146fbbe5ebb558b649f30
Paul Aglipay <paglipay@gmail.com>
2024-01-16 13:22:09 -0800

    sudo docker run --rm -it -p 80:5000/tcp paglipay/spring-demo:latest

--- terraform/instances.tf
+++ terraform/instances.tf
@@ -38,8 +38,13 @@ resource "aws_instance" "nginx1" {
   }
 
   provisioner "file" {
-    source      = "../spring-demo.zip"
-    destination = "/home/ec2-user/spring-demo.zip"
+    source      = "../spring-demo"
+    destination = "/home/ec2-user/spring-demo"
+  }
+
+  provisioner "file" {
+    source      = "../Dockerfile"
+    destination = "/home/ec2-user/Dockerfile"
   }
 
   # add remote exec configuration
@@ -53,12 +58,19 @@ resource "aws_instance" "nginx1" {
       # "ansible-playbook -i ansible/hosts ansible/playbook.yml",
       # "chmod +x /home/ec2-user/userdata.sh",
       # "sh /home/ec2-user/userdata.sh",
-      "sudo yum install -y maven docker",
+      # "sudo yum install -y maven",
       # "unzip /spring-demo.zip",  # Assuming project is transferred beforehand
       # "cd spring-demo",
       # "mvn clean install",
-      "docker build -t spring-demo .",
-      "docker run --rm -it -p 5000:5000/tcp paglipayspring-demo:latest"
+      "sudo yum install -y docker",
+      "sudo usermod -a -G docker ec2-user",
+      "sudo yum install -y python3-pip",
+      # "sudo pip3 install -y docker-compose",
+      "sudo systemctl enable docker.service",
+      "sudo systemctl start docker.service",
+
+      "sudo docker build -t paglipay/spring-demo:latest .",
+      "sudo docker run --rm -it -p 80:5000/tcp paglipay/spring-demo:latest"
     ]
     on_failure = continue
   }
@@ -81,13 +93,59 @@ resource "aws_instance" "nginx2" {
   subnet_id              = aws_subnet.public_subnet2.id
   vpc_security_group_ids = [aws_security_group.nginx_sg.id]
 
-  user_data = <<EOF
-#! /bin/bash
-sudo amazon-linux-extras install -y nginx1
-sudo service nginx start
-sudo rm /usr/share/nginx/html/index.html
-echo '<html><head><title>Taco Team Server 2</title></head><body style=\"background-color:#1F778D\"><p style=\"text-align: center;\"><span style=\"color:#FFFFFF;\"><span style=\"font-size:28px;\">You did it! Have a &#127790;</span></span></p></body></html>' | sudo tee /usr/share/nginx/html/index.html
-EOF
+  # include aws aws_key_pair.deployer.key_name
+  key_name = "aws_rsa"
+
+  connection {
+    type        = "ssh"
+    user        = "ec2-user"
+    private_key = file("aws_rsa.pem")
+    host        = aws_instance.nginx1.public_ip
+  }
+
+  provisioner "file" {
+    source      = "../ansible"
+    destination = "/home/ec2-user/ansible"
+  }
+
+  provisioner "file" {
+    source      = "../templates/userdata.sh"
+    destination = "/home/ec2-user/userdata.sh"
+  }
+
+  provisioner "file" {
+    source      = "../spring-demo"
+    destination = "/home/ec2-user/spring-demo"
+  }
+
+  provisioner "file" {
+    source      = "../Dockerfile"
+    destination = "/home/ec2-user/Dockerfile"
+  }
+
+  # add remote exec configuration
+  provisioner "remote-exec" {
+
+    inline = [
+      "sudo yum update -y",
+      "sudo yum install -y docker",
+      "sudo usermod -a -G docker ec2-user",
+      "sudo yum install -y python3-pip",
+      "sudo systemctl enable docker.service",
+      "sudo systemctl start docker.service",
+      "sudo docker build -t paglipay/spring-demo:latest .",
+      "sudo docker run --rm -it -p 80:5000/tcp paglipay/spring-demo:latest"
+    ]
+    on_failure = continue
+  }
+
+#   user_data = <<EOF
+# #! /bin/bash
+# sudo amazon-linux-extras install -y nginx1
+# sudo service nginx start
+# sudo rm /usr/share/nginx/html/index.html
+# echo '<html><head><title>Taco Team Server 1</title></head><body style=\"background-color:#1F778D\"><p style=\"text-align: center;\"><span style=\"color:#FFFFFF;\"><span style=\"font-size:28px;\">You did it! Have a &#127790;</span></span></p></body></html>' | sudo tee /usr/share/nginx/html/index.html
+# EOF
 
   tags = local.common_tags
 
